54473fbcdb7af35f0360518c2834ee8e96344976
whiskpo0 <whiskypo082@gmail.com>
2019-02-12 11:12:12 -0600

    initial commit of JS Pig-game

---
+++ scripts.js
@@ -0,0 +1,37 @@
+/******************************************
+ * Variables and data types
+ */
+/*
+var firstName = 'John'; 
+console.log(firstName); 
+
+var lastname = 'Smith'; 
+var age = 48; 
+
+var fullage = true; 
+console.log(fullage); 
+
+var job; 
+console.log(job); 
+
+
+job = 'Teacher'; 
+console.log(job); 
+*/
+
+/******************************************
+ * Variables mutation and type coercion
+ */
+
+ var firstName = 'John'; 
+ var age = 28; 
+
+ console.log(firstName + ' ' + age); 
+
+ var jobm, isMarried; 
+ job = 'teacher'; 
+ isMarried = false; 
+
+ console.log(firstName + 'is a ' + age + 'years old' + job + '. Is he married? ' + isMarried); 
+
+
